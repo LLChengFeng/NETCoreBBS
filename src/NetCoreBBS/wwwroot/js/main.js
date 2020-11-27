52948eab8162d84af0374cc8a4e76076c27c3362
IvesYang <ivesyang@acadsoc.com>
2020-11-27 18:45:29 +0800

    添加2d动画人物

---
+++ src/NetCoreBBS/wwwroot/js/main.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    L2Dwidget.init({
+        model: {
+            jsonPath: 'https://unpkg.com/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json',
+        },
+        display: {
+            superSample: 1.5,
+            width: 150,
+            height: 300,
+            position: 'right',
+            hOffset: 0,
+            vOffset: -20,
+        },
+        mobile: {
+            show: true,
+            scale: 0.5,
+            motion: true,
+        },
+        react: {
+            opacityDefault: 0.7,
+            opacityOnHover: 0.2,
+        }
+    });
+});
