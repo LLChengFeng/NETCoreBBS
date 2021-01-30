2e7af839423315d88fc5fe1b621319fccf3f1ece
IvesYang <ivesyang@acadsoc.com>
2021-01-30 17:40:26 +0800

    添加MenoryQueue

---
+++ NETCoreBBS.EventBus.MemoryQueue/InMemoryEventBus.cs
@@ -0,0 +1,43 @@
+﻿using NETCoreBBS.EventBus.Abstractions;
+using NETCoreBBS.EventBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NETCoreBBS.EventBus.MemoryQueue
+{
+    public class InMemoryEventBus : IEventBus
+    {
+        public Task PublishAsync(IntegrationEvent @event, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeDynamic<TH>(string eventName) where TH : IIntegrationEventHandler
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnsubscribeDynamic<TH>(string eventName) where TH : IIntegrationEventHandler
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
